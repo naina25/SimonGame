7a1b69f5a0a499a24201e6529ccafab537fad775
naina <nanu02d8@gmail.com>
2020-10-18 13:38:36 +0530

    Updated h1 after keypress event

--- actions.js
+++ actions.js
@@ -2,6 +2,17 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 let userClickedPattern = [];
+let startGame = false;
+let level = 0;
+
+$(document).on("keypress", function()
+{
+    if (startGame === false)
+    {
+        nextSequence();
+        startGame = true;
+    }
+});
 
 $(".btn").on('click',function(){
     var userChosenColor = this.id;
@@ -16,14 +27,14 @@ function animatePress(currentColour){
         $('#' + currentColour).removeClass("pressed");
     },100);
 }
-
-
 function nextSequence() 
 {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+    level++;
+    $('h1').text(`Level ${level}`);
 }
 function playSound(name){
     var audio = new Audio(`sounds/${name}.mp3`);
